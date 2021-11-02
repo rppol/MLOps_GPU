1044e39c06ac22a26ce0ec85f74784b8eee01e6a
rutik <rppol1412@gmail.com>
2021-11-02 11:47:07 +0000

    added test

--- src/split_data.py
+++ src/split_data.py
@@ -1,7 +1,7 @@
 import os
 import argparse
 from dask_ml.model_selection import train_test_split
-from load_data import load_data
+from load_data import load_train_data
 from feature_engg import feature_engg
 from dask_client import read_params
 import xgboost as xgb
@@ -10,8 +10,8 @@ def split_data(client, df, config_path):
     config = read_params(config_path)
     split_ratio = config["split_data"]["valid_test_ratio"]
     random_state = config["base"]["random_state"]
-    y = df['fare_amount']
-    X = df.drop(['fare_amount'], axis=1)
+    y = df[config["base"]["target_col"]]
+    X = df.drop([config["base"]["target_col"]], axis=1)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=split_ratio, random_state=random_state)
     dtrain, dvalid = generate_Dmatrix(client, X_train, y_train, X_test, y_test)
     return (client, dtrain, dvalid)
@@ -25,6 +25,6 @@ if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
-    client, df = load_data(config_path=parsed_args.config)
+    client, df = load_train_data(config_path=parsed_args.config)
     client, df = feature_engg(client, df)
     split_data(client, df,config_path=parsed_args.config)
