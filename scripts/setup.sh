a4532cca8288f7ef77cda054e985048f28a0f569
rutik <rppol1412@gmail.com>
2021-11-05 08:07:44 +0000

    jenkins edit

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-wget https://repo.anaconda.com/miniconda/Miniconda3-py38_4.10.3-Linux-x86_64.sh
-chmod +x Miniconda3-py38_4.10.3-Linux-x86_64.sh
-./Miniconda3-py38_4.10.3-Linux-x86_64.sh -b -u
+export PATH="/home/jenkins/miniconda/bin:$PATH"
 conda create -n mlops -c rapidsai -c nvidia -c conda-forge rapids=21.10 python=3.8 cudatoolkit=11.2
