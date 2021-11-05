298ea0b962a245a0312dbbdbe518704b25ea3357
rutik <rppol1412@gmail.com>
2021-11-05 09:03:37 +0000

    jenkins edit

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,3 +2,7 @@
 export PATH="/home/jenkins/miniconda/bin:$PATH"
 source ~/.bashrc 
 conda activate mlops
+pip istall dask-ml
+pip install sklearn
+pip install pandas
+python /home/nvidiatest/mlops_blog/src/test_and_evaluate.py
