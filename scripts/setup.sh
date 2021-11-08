72bedbd22fc299fb43b3fb12f3d108cdbd1a2761
rutik <rppol1412@gmail.com>
2021-11-08 16:16:42 +0000

    Jenkins test

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-export PATH="/home/jenkins/miniconda/bin:$PATH"
-source ~/.bashrc 
 conda activate mlops
-pip install dask-ml
-python /home/nvidiatest/mlops_blog/src/test_and_evaluate.py
+pwd
